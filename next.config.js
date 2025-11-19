eea8b5575b89b0537176f9a292c8503bbb5dc85b
Kumar-Saurabh-Tiwari <tiwarisaurabhkumar0@gmail.com>
2025-11-19 17:10:51 +0530

    code features added

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
   },
+  webpack: (config, { isServer }) => {
+    return config;
+  },
 }
 
 module.exports = nextConfig
